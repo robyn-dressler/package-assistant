64252aa93f0bbea36cc3e9d372cb79af5d62d6e3
Robyn Dressler <robyn.dressler@gmail.com>
2024-10-24 14:33:41 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+mod config;
+
+#[derive(Debug, Parser)]
+#[command(name = "package-assistant")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Init {
+        #[arg(long = "config", short = 'c')]
+        config: Option<PathBuf>,
+        #[arg(long = "service", short = 's')]
+        service: bool,
+    },
+    Changes {
+        #[arg(long = "pending-only", short = 'p')]
+        pending_only: bool,
+        #[arg(long = "query", short = 'q')]
+        query: Option<String>,
+    },
+    CheckUpdates {
+        #[arg(long = "download", short = 'd')]
+        download: bool,
+    },
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Command::Init {
+            config: path_opt,
+            service,
+        } => {
+            let config_result = config::init(path_opt);
+            match config_result {
+                Err(config_error) => {
+                    println!("Error: {}", config_error);
+                }
+                Ok(config_dir) => {
+                    if let Some(s) = config_dir.to_str() {
+                        println!("Wrote configuration to {}", s)
+                    }
+                }
+            };
+        }
+        Command::Changes {
+            pending_only,
+            query,
+        } => {
+            let pending_str = if pending_only { " pending " } else { " " };
+            let query_str = if let Some(q) = query {
+                format!("for query \"{}\"", q)
+            } else {
+                String::from("")
+            };
+
+            println!("Getting all{}changes {}", pending_str, query_str);
+        }
+        Command::CheckUpdates { download } => {
+            println!("Checking for updates!");
+            if download {
+                println!("Downloading available packages...");
+            }
+        }
+    }
+}
